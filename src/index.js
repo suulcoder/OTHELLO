9664f712effd7af4824ebcfe6b6bb4ef26202187
suulcoder <ef.proyectomundial@gmail.com>
2020-01-31 08:57:42 -0600

    logic done

--- src/index.js
+++ src/index.js
@@ -31,12 +31,13 @@ const renderCell = ({
 	}
 
 	piece.onclick = () => {
+		const { board } = state;
 		if(value===2){
-			state.board[index[0]][index[1]] = (state.turn) ? 1 : -1;
+			board[index[0]][index[1]] = (state.turn) ? 1 : -1;
 			let control = 1;
 			let boolcontrol = (index[0]+control>0 && index[0]+control<8) ? true : false;
-			while(boolcontrol && state.board[index[0]+control][index[1]]!=state.board[index[0]][index[1]] && state.board[index[0]+control][index[1]]!=0 && state.board[index[0]+control][index[1]]!=2){
-				state.board[index[0]+control][index[1]] = state.board[index[0]][index[1]]
+			while(boolcontrol && board[index[0]+control][index[1]]!=board[index[0]][index[1]] && board[index[0]+control][index[1]]!=0 && board[index[0]+control][index[1]]!=2){
+				board[index[0]+control][index[1]] = board[index[0]][index[1]]
 				if (index[0]+control+1>=0 && index[0]+control+1<8){
 					control = control + 1
 				}
@@ -45,21 +46,9 @@ const renderCell = ({
 				}
 			}
 			control = 1;
-			boolcontrol = (index[0]-control>0 && index[0]-control<8) ? true : false;
-			while(boolcontrol && state.board[index[0]-control][index[1]]!=state.board[index[0]][index[1]] && state.board[index[0]-control][index[1]]!=0 && state.board[index[0]-control][index[1]]!=2){
-				state.board[index[0]-control][index[1]] = state.board[index[0]][index[1]]
-				if (index[0]-control+1>=0 && index[0]-control+1<8){
-					control = control + 1
-				}
-				else{
-					boolcontrol = false;
-				}
-			}
-			control = 1;
 			boolcontrol = (index[1]+control>0 && index[1]+control<8) ? true : false;
-			console.log(state.board[index[0]])
-			while(boolcontrol && state.board[index[0]][index[1]+control]!=state.board[index[0]][index[1]] && state.board[index[0]][index[1]+control]!=0 && state.board[index[0]][index[1]+control]!=2){
-				state.board[index[0]][index[1]+control] = state.board[index[0]][index[1]]
+			while(boolcontrol && board[index[0]][index[1]+control]!=board[index[0]][index[1]] && board[index[0]][index[1]+control]!=0 && board[index[0]][index[1]+control]!=2){
+				board[index[0]][index[1]+control] = board[index[0]][index[1]]
 				if (index[1]+control+1>=0 && index[1]+control+1<8){
 					control = control + 1
 				}
@@ -69,8 +58,8 @@ const renderCell = ({
 			}
 			control = 1;
 			boolcontrol = (index[1]-control>0 && index[1]-control<8) ? true : false;
-			while(boolcontrol && state.board[index[0]][index[1]-control]!=state.board[index[0]][index[1]] && state.board[index[0]][index[1]-control]!=0 && state.board[index[0]][index[1]-control]!=2){
-				state.board[index[0]][index[1]-control] = state.board[index[0]][index[1]]
+			while(boolcontrol && board[index[0]][index[1]-control]!=board[index[0]][index[1]] && board[index[0]][index[1]-control]!=0 && board[index[0]][index[1]-control]!=2){
+				board[index[0]][index[1]-control] = board[index[0]][index[1]]
 				if (index[1]-control+1>=0 && index[1]-control+1<8){
 					control = control + 1
 				}
@@ -78,19 +67,69 @@ const renderCell = ({
 					boolcontrol = false;
 				}
 			}
+			try{
+				control = 1;
+				boolcontrol = (index[0]-control>=0 && index[0]-control<8) ? true : false;
+				while(boolcontrol && board[index[0]-control][index[1]]!=board[index[0]][index[1]] && board[index[0]-control][index[1]]!=0 && board[index[0]-control][index[1]]!=2){
+					board[index[0]-control][index[1]] = board[index[0]][index[1]]
+					if (index[0]-control+1>=0 && index[0]-control+1<8){
+						control = control + 1
+					}
+					else{
+						boolcontrol = false;
+					}
+				}
+			}
+			catch(error){
+				console.log(error);
+			}
+			
 			if(index[0]+1>=0 && index[0]+1<8){
-				state.board[index[0]+1][index[1]] = (state.board[index[0]+1][index[1]]==0 && state.board[index[0]+1][index[1]]!=1 && state.board[index[0]+1][index[1]]!=-1) ? 2 : state.board[index[0]+1][index[1]];
+				board[index[0]+1][index[1]] = (board[index[0]+1][index[1]]==0 && board[index[0]+1][index[1]]!=1 && board[index[0]+1][index[1]]!=-1) ? 2 : board[index[0]+1][index[1]];
 			}				
 			if(index[0]-1>=0 && index[0]-1<8){
-				state.board[index[0]-1][index[1]] = (state.board[index[0]-1][index[1]]==0 && state.board[index[0]-1][index[1]]!=1 && state.board[index[0]-1][index[1]]!=-1) ? 2 : state.board[index[0]-1][index[1]];	
+				board[index[0]-1][index[1]] = (board[index[0]-1][index[1]]==0 && board[index[0]-1][index[1]]!=1 && board[index[0]-1][index[1]]!=-1) ? 2 : board[index[0]-1][index[1]];	
 			}
 			if(index[1]+1>=0 && index[1]+1<8){
-				state.board[index[0]][index[1]+1] = (state.board[index[0]][index[1]+1]==0 && state.board[index[0]][index[1]+1]!=1 && state.board[index[0]][index[1]+1]!=-1) ? 2 : state.board[index[0]][index[1]+1];	
+				board[index[0]][index[1]+1] = (board[index[0]][index[1]+1]==0 && board[index[0]][index[1]+1]!=1 && board[index[0]][index[1]+1]!=-1) ? 2 : board[index[0]][index[1]+1];	
 			}
 			if(index[1]-1>=0 && index[1]-1<8){
-				state.board[index[0]][index[1]-1] = (state.board[index[0]][index[1]-1]==0 && state.board[index[0]][index[1]-1]!=1 && state.board[index[0]][index[1]-1]!=-1) ? 2 : state.board[index[0]][index[1]-1];	
+				board[index[0]][index[1]-1] = (board[index[0]][index[1]-1]==0 && board[index[0]][index[1]-1]!=1 && board[index[0]][index[1]-1]!=-1) ? 2 : board[index[0]][index[1]-1];	
 			}
 			state.turn = !state.turn;
+			let whites = 0;
+			let blacks = 0;
+			let end_of_game = 0;
+			board.map((row,irow) => {
+				row.map((column,icolumn) => {
+					if(column==1){
+						whites = whites + 1;
+					}
+					else if(column==-1){
+						blacks = blacks + 1;
+					}
+					else if(column==2){
+						end_of_game = end_of_game + 1;
+					}
+				})
+			})
+			let message = '';
+			if(end_of_game==0){
+				message = (whites>0) ? 'WHTE HAVE WON' : 'BLACK HAVE WON'
+				state = {
+					turn: true,
+					board: [[0,0,0,0,0,0,0,0],
+							[0,0,0,0,0,0,0,0],
+							[0,0,0,2,2,0,0,0],
+							[0,0,2,1,-1,2,0,0],
+							[0,0,2,-1,1,2,0,0],
+							[0,0,0,2,2,0,0,0],
+							[0,0,0,0,0,0,0,0],
+							[0,0,0,0,0,0,0,0]
+						]
+				};
+				alert(message)
+			}
 			root.innerHTML = '';
         	render(root, state);
 		}
@@ -105,8 +144,10 @@ const render = (root, state) =>{
 	header.style.width = '100%';
 	header.style.height = '120px';
 
-	const header_tittle = document.createElement('h1',null,'OTHELLO');
+	const tittle = 'OTHELLO'
+	const header_tittle = document.createElement('div',{tittle}	);
 	header_tittle.style.backgroundColor = 'black'
+	header_tittle.style.color = 'white'
 	header_tittle.style.fontSize = '40px';
 	header.appendChild(header_tittle)
 
